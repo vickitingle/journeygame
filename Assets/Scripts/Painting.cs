8c69249b44d1cc9d9d4437b7f0fe2dd33d148bf6
Vicki <vicki.tingle@gmail.com>
2019-04-29 15:18:18 +0100

    Test initial commit.

---
+++ Assets/Scripts/Painting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Painting : MonoBehaviour {
+
+public string blurbText;
+public string artistText;
+UnityEngine.UI.Text textObject;
+UnityEngine.UI.Text artistTextObj;
+
+UnityEngine.UI.Text displayObj;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+    var can = GameObject.Find("ArtDescCanvas");
+    var textComp = can.transform.Find("ArtDescription").gameObject;
+    textObject = textComp.GetComponent<UnityEngine.UI.Text>();
+    textObject.text = blurbText;
+
+    var artistTextComp = can.transform.Find("ArtistAndTitle").gameObject;
+    artistTextObj = artistTextComp.GetComponent<UnityEngine.UI.Text>();
+    artistTextObj.text = artistText;
+  }
+
+  void OnTriggerExit(Collider other) {
+    textObject.text = "";
+    artistTextObj.text = "";
+  }
+
+}
---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text : MonoBehaviour {
+
+public string displayText;
+UnityEngine.UI.Text displayObj;
+
+
+	// Use this for initialization
+	void Start () {
+    
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
